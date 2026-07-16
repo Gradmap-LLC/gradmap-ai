45d97cfdfe205c427f0a0c60676a6af35bc73602
DerenDesign <67876073+DerenDesign@users.noreply.github.com>
2026-07-17 00:31:40 +0300

    added 2nd db

--- contextv2.py
+++ contextv2.py
@@ -9,11 +9,21 @@ from recommend import recomendations
 
 
 DB_CONFIG = {
+    "student": {
     "host": os.environ.get("GM_DB_HOST", "3.14.196.144"),
     "port": int(os.environ.get("GM_DB_PORT", "5432")),
     "user": os.environ.get("GM_DB_USER", "deren_readonly"),
     "password": os.environ.get("GM_DB_PASSWORD", "derenreadonly@3sd1"),
     "dbname": os.environ.get("GM_DB_NAME", "student"),
+    },
+    "gm_schools": {
+    "host": os.environ.get("GM_DB_HOST", "3.14.196.144"),
+    "port": int(os.environ.get("GM_DB_PORT", "5432")),
+    "user": os.environ.get("GM_DB_USER", "deren_readonly"),
+    "password": os.environ.get("GM_DB_PASSWORD", "derenreadonly@3sd1"),
+    "dbname": os.environ.get("GM_DB_NAME", "gm_schools"),
+    }
+
 }
 
 
